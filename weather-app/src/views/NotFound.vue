3b5ba3760157ac635ab97d2a729c579d39ccf917
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-25 01:50:37 +0100

    Added Not Found Page

--- weather-app/src/views/NotFound.vue
+++ weather-app/src/views/NotFound.vue
@@ -1,5 +1,39 @@
+<script lang="ts" setup></script>
+
 <template>
-    <p>Hello Vue, Not Found View</p>
+    <div class="container">
+        <img src="/youcan.png" alt="YouCan" class="logo" />
+        <h1>Error 404...Oups !</h1>
+        <p>Page not Found, Please Go Back to Home !</p>
+
+        <router-link to="/" class="link">Go Home</router-link>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+
+<style scoped>
+.container {
+    width: 100%;
+    height: 95vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+    color: white;
+}
+
+.logo {
+    width: 200px;
+    height: 200px;
+}
+
+.link {
+    background-color: #b8266e;
+    text-decoration: none;
+    padding: 10px 30px;
+    border-radius: 10px;
+    color: white;
+    margin-top: 20px;
+}
+</style>
